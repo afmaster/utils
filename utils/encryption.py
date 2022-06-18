7e6b54808da0f3eefec150490a6bf1d29b54c504
André Cerutti Franciscatto <andre@franciscatto.com>
2022-06-18 17:52:35 -0300

    SHA256
    hashing

--- utils/encryption.py
+++ utils/encryption.py
@@ -4,6 +4,11 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from hashlib import sha256
+
+
+def hashing(text):
+    return sha256(text.encode('utf-8')).hexdigest()
 
 
 def create_salt() -> bytes:
